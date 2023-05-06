c56d1d8bf50ee652eb7a4ee788f33e26501c25f0
João Paulo BS <ze.kiefa@gmail.com>
2023-05-06 20:04:24 -0300

    Adjusting fields not mappeds

--- kotlin-quarkus/src/main/kotlin/br/com/cvc/evaluation/service/mapper/HotelMapper.kt
+++ kotlin-quarkus/src/main/kotlin/br/com/cvc/evaluation/service/mapper/HotelMapper.kt
@@ -4,7 +4,8 @@ import br.com.cvc.evaluation.broker.dto.BrokerHotel
 import br.com.cvc.evaluation.domain.Hotel
 import org.mapstruct.Mapper
 
-@Mapper
+@Mapper(uses = [RoomMapper::class])
 interface HotelMapper {
     fun toDomain(brokerHotel: BrokerHotel): Hotel
+
 }
--- kotlin-quarkus/src/main/kotlin/br/com/cvc/evaluation/service/mapper/RoomMapper.kt
+++ kotlin-quarkus/src/main/kotlin/br/com/cvc/evaluation/service/mapper/RoomMapper.kt
@@ -3,8 +3,11 @@ package br.com.cvc.evaluation.service.mapper
 import br.com.cvc.evaluation.broker.dto.BrokerHotelRoom
 import br.com.cvc.evaluation.domain.Room
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 
 @Mapper
 interface RoomMapper {
+    @Mapping(target = "totalPrice", ignore = true)
+    @Mapping(target = "priceDetail", ignore = true)
     fun toDomain(brokerHotelRoom: BrokerHotelRoom): Room
 }
